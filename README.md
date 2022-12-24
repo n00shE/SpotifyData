a1aaddfd827979fba84dd5bb8a60abcb694918b5
n00shE <ryanblanchard00@gmail.com>
2022-12-23 17:11:49 -0800

    rework

--- README.md
+++ README.md
@@ -1,49 +1,63 @@
 ﻿# SpotifyData
  
 ## Prerequisites
-You must download your data package from Spotify, this can take 1-3 weeks for Spotify to deliver.   
+You must download your data package from Spotify, this can take 1-4 weeks for Spotify to deliver.   
+This program supports both the account data download and the extended history download.   
 https://www.spotify.com/us/account/privacy/
  
 ## Usage
 ```
-usage: spotifyparse.py [-h] [-s] [-o OUTFILE] [-p] path
+usage: spotifyparse.py [-h] [-s] [-e] [-i] [-o OUTFILE] [-d] [-n PLOTNUM] path
 
-Parses spotify data downloads
+Parses and charts Spotify data downloads
 
 positional arguments:
-  path                  Path to the folder containing StreamingHistory files
+  path                  Path to the folder containing json files
 
 optional arguments:
   -h, --help            show this help message and exit
   -s, --song            Gather by song instead of artist
+  -e, --extended        Use the lifetime streaming history files
+  -i, --include-podcasts
+                        Include podcast data, only works on extended data
   -o OUTFILE, --outfile OUTFILE
-                        Saves csv in current directory with specified name (defaults to out.csv)
-  -p, --plot            Create a matplotlib bar chart of the top 20 by playtime
+                        Saves csv in current directory with specified name
+  -d, --dark-mode       Plot with a dark background
+  -n PLOTNUM, --plot-num PLOTNUM
+                        Number of items to plot
 ```
 
-## Methods
-
-### searchType
-searchType is the way the dictionary of playtime is keyed. Anytime searchType is mentioned, 'trackName' or 'artistName' are the possible inputs.   
-'trackName' gathers playtime by song and 'artistName' gathers playtime by artist.   
+## SpotifyData Class
+```
+SpotifyData(path: str, searchType: str, extended=False, includePodcasts=False, darkMode=False)
+```
+The class requires two arguments with three optionals.   
+Class must be instantiated before using the methods below.    
+### Required Arguments
+The path to the folder with the *StreamingHistoryX.json* files or the the *endsong_X.json* files.   
+The search type which is either *'trackName'* or *'artistName'*.   
+### Optional Arguments
+Boolean if the data is from the extended streaming history download.  (The defualt is the account data download)   
+Boolean to include podcasts. (Only works with the extended stream history)   
+Boolean to make the chart dark mode.   
 
 ### create_playtime_dict()
 ```
-create_playtime_dict(path: str, searchType: str)
+@property
+SpotifyData.create_playtime_dict()
 ```
-Creates dictionary of playtime in minutes, keyed by artist or song depending on 'searchType' ('trackName' or 'artistName').  
-Requires a searchType ('trackName' or 'artistName') and the path to the folder containing the *StreamingHistoryX.json* files.   
+Creates dictionary of playtime in minutes, keyed by artist or song name depending on the search type.   
 
 ### create_csv()
 ```
-create_csv(playtime: dict, searchType: str, file: str)
+SpotifyData.create_csv(file: str)
 ```
-Creates csv with 'searchType' and playtime (in minutes) columns.   
-Requires a playtime dictionary (gathered from create_playtime_dict()), a searchType (see above), and the file name/relative path to save CSV.
+Creates csv with playtime (in minutes) by artist or song name.   
+Requires the file name/relative path to save CSV.
 
 ### plot_data()
 ```
-plot_data(playtime: dict, searchType: str)
+SpotifyData.plot_data(plotNum: int)
 ```
-Plots top 20 with matplotlib.   
-Requires a playtime dictionary and a searchType (see above).   
+Plots top plotNum artists or song names with matplotlib.   
+Requires the number of items to plot.
--- spotifyparse.py
+++ spotifyparse.py
@@ -1,91 +1,182 @@
 """
 Ryan Blanchard
-rmblanch@uci.edu
 Spotify data download parser
 """
-
 
 import json
 import argparse
 import csv
 import os
-#import time
+from datetime import datetime
+from collections import OrderedDict, defaultdict
+from itertools import islice
 
-def create_playtime_dict(path: str, searchType: str):
-    '''
-    Creates dictionary of playtime in minutes, keyed by artist or song depending on 'searchType' ('trackName' or 'artistName').  
-    Requires a search type ('trackName' or 'artistName') and the path to the folder containing the StreamingHistoryX.json files.  
-    '''
-    if searchType != 'trackName' and searchType != 'artistName':
-        print("ERROR: Invalid search type.")
-        quit()
-    playtime = {}
-    for jsonFile in os.scandir(path):
-        if "StreamingHistory" in jsonFile.path:
-            print('Opening ' + jsonFile.path + '...')
-            with open(jsonFile.path, encoding="utf8") as file:
-                songList = json.load(file)
+class SpotifyData:
+    def __init__(self, path: str, searchType: str, extended=False, includePodcasts=False, darkMode=False):
+        self.path = path
+        self.searchType = searchType
+        self.friendlySearchType = searchType
+        self.extended = extended
+        self.includePodcasts = includePodcasts
+        self.darkMode = darkMode
+        self.playtime = None
+        self.newest = None
+        self.oldest = None
+        if searchType != 'trackName' and searchType != 'artistName':
+            print("ERROR: Invalid search type.")
+            quit(1)
+        if extended:
+            if searchType == 'trackName':
+                self.searchType = 'master_metadata_track_name'
+            elif searchType == 'artistName':
+                self.searchType = 'master_metadata_album_artist_name'
+
+    @property
+    def playtime_dict(self):
+        '''
+        Creates dictionary of playtime in minutes, keyed by artist or song depending on 'searchType' ('trackName' or 'artistName').  
+        Requires a search type ('trackName' or 'artistName') and the path to the folder containing the StreamingHistoryX.json files.  
+        '''
+        if self.playtime:
+            return self.playtime
+        self.playtime = defaultdict(int)
+        if self.extended:
+            self.parse_extended_data_files()
+        else:
+            self.parse_account_data_files()
+        self.playtime = OrderedDict(sorted(self.playtime.items(), key=lambda item: item[1], reverse=True))
+        return self.playtime
+
+    def parse_extended_data_files(self):
+        '''
+        Parse the lifetime streaming data into a playtime dictionary
+        '''
+        files = 0
+        for jsonFile in os.scandir(self.path):
+            if 'endsong' in jsonFile.path and 'json' in jsonFile.path:
+                print('Opening ' + jsonFile.path + '...')
+                files += 1
+                with open(jsonFile.path, encoding="utf8") as file:
+                    songList = json.load(file)
                 for song in songList:
-                    #if '$' in song[searchType]: continue
-                    try:
-                        playtime[song[searchType]] += song['msPlayed'] / 60000 # divided to convert ms to minutes.
-                    except KeyError:
-                        playtime[song[searchType]] = song['msPlayed'] / 60000
-    return dict(sorted(playtime.items(), key=lambda item: item[1], reverse=True))
+                    self.find_date_range(song['ts'])
+                    if song[self.searchType] == None:
+                        if self.includePodcasts:
+                            if self.friendlySearchType == 'artistName':
+                                self.playtime[song['episode_show_name']] += song['ms_played'] / 60000 # divided to convert ms to minutes.
+                            else:
+                                self.playtime[song['episode_name']] += song['ms_played'] / 60000 # divided to convert ms to minutes.
+                    else:
+                        self.playtime[song[self.searchType]] += song['ms_played'] / 60000 # divided to convert ms to minutes.
+        if files == 0:
+            exit("ERROR: No endsong_x.json files found!")
+        else:
+            print(f"Processed {files} files.")
 
-def create_csv(playtime: dict, searchType: str, file: str):
-    '''
-    Creates csv with 'searchType' and playtime (in minutes) columns.
-    Requires a playtime dictionary (gathered from create_playtime_dict()), a searchType (see above), and the file name/relative path to save CSV.
-    '''
-    file = "out.csv"
-    with open(file, 'w', newline="\n", encoding="utf8") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow([searchType, 'playtime(m)'])
-        for k, v in playtime.items():
-            writer.writerow([k, v])
-    print(f'Finished writing csv file to: {file}')
+    def parse_account_data_files(self):
+        '''
+        Parse the account data download into a playtime dictionary
+        '''
+        files = 0
+        for jsonFile in os.scandir(self.path):
+            if "StreamingHistory" in jsonFile.path and 'json' in jsonFile.path:
+                print('Opening ' + jsonFile.path + '...')
+                files += 1
+                with open(jsonFile.path, encoding="utf8") as file:
+                    songList = json.load(file)
+                for song in songList:
+                    self.find_date_range(song['endTime'])
+                    #if '$' in song[self.searchType]: continue
+                    self.playtime[song[self.searchType]] += song['msPlayed'] / 60000 # divided to convert ms to minutes.
+        if files == 0:
+            exit("ERROR: No StreamingHistoryx.json files found!")
+        else:
+            print(f"Processed {files} files.")
 
-def plot_data(playtime: dict, searchType: str):
-    '''
-    Plots top 20 with matplotlib.
-    Requires a playtime dictionary and a searchType (see above).
-    '''
-    import matplotlib.pyplot as plt
-    l = dict(list(playtime.items())[:20])
-    #print(l)
-    plt.style.use('ggplot')
-    x_pos = [i for i, _ in enumerate(l.keys())]
+    def find_date_range(self, time: str):
+        '''
+        Find the date range of the dataset
+        '''
+        if self.extended:
+            date = datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ')
+        else:
+            date = datetime.strptime(time, '%Y-%m-%d %H:%M')
+        
+        if not self.oldest or date < self.oldest:
+            self.oldest = date
+        if not self.newest or date > self.newest:
+            self.newest = date
 
-    plt.bar(x_pos, l.values(), color='blue')
-    #plt.xlabel(searchType)
-    plt.ylabel("Minutes")
-    plt.title(f"Playtime by {searchType}")
+    def create_csv(self, file='out.csv'):
+        '''
+        Creates csv with 'searchType' and playtime (in minutes) columns.
+        Requires a playtime dictionary (gathered from create_playtime_dict()), a searchType, and the file name/relative path to save CSV.
+        '''
+        with open(file, 'w', newline="\n", encoding="utf8") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([self.friendlySearchType, 'playtime(m)'])
+            for k, v in self.playtime_dict.items():
+                writer.writerow([k, v])
+        print(f'Finished writing csv file to: {file}')
 
-    plt.xticks(x_pos, l.keys(),
-    rotation=45, 
-    horizontalalignment='right',
-    fontweight='light',
-    fontsize='small')
+    def plot_data(self, toPlot: int):
+        '''
+        Plots top 20 with matplotlib.
+        Requires a playtime dictionary and a searchType.
+        '''
+        import matplotlib.pyplot as plt
+        #l = dict(list(self.playtime_dict.items())[:toPlot])
+        sliced = islice(self.playtime_dict.items(), toPlot)
+        truncDict = OrderedDict(sliced)
+        roundedTruncDict = dict(zip(truncDict, map(round, truncDict.values())))
 
-    plt.tight_layout()
-    plt.show()
+        if self.darkMode:
+            plt.style.use('dark_background')
+        else:
+            plt.style.use('ggplot')
+        fig, ax = plt.subplots()
+        fig.set_size_inches(10, 6)
+        #fig.set_dpi(100)
+
+        x_pos = [i for i, _ in enumerate(truncDict.keys())]
+
+        bars = ax.bar(x_pos, truncDict.values(), color='blue')
+        #ax.set_xlabel(self.friendlySearchType)
+        ax.set_ylabel("Minutes")
+        ax.bar_label(bars, roundedTruncDict.values())
+        #format = "%m/%d/%y"
+        ax.set_title(f"Top {toPlot} playtime by {self.friendlySearchType} from {self.oldest} to {self.newest}")
+
+        ax.set_xticks(x_pos, truncDict.keys(),
+        rotation=45, 
+        horizontalalignment='right',
+        fontweight='light',
+        fontsize='small')
+
+        plt.tight_layout()
+        plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='spotifyparse.py',
-                                    description='Parses spotify data downloads')
-    parser.add_argument('path', action='store', type=str, help="Path to the folder containing StreamingHistory files")
+                                    description='Parses and charts Spotify data downloads')
+    parser.add_argument('path', action='store', type=str, help="Path to the folder containing json files")
     parser.add_argument('-s','--song', action='store_true', help="Gather by song instead of artist")
-    parser.add_argument('-o','--outfile', action='store', default='out.csv', help="Saves csv in current directory with specified name (defaults to out.csv)")
-    parser.add_argument('-p','--plot', action='store_true', help="Create a matplotlib bar chart of the top 20 by playtime")
+    #parser.add_argument('-p','--plot', action='store_true', help="Create a matplotlib bar chart of the top 20 by playtime")
+    parser.add_argument('-e','--extended', action='store_true', help="Use the extended streaming history files")
+    parser.add_argument('-i','--include-podcasts', action='store_true', help="Include podcast data, only works on extended data", dest='includePodcasts')
+    parser.add_argument('-o','--outfile', action='store', default=None, help="Saves csv in current directory with specified name")
+    parser.add_argument('-d','--dark-mode', action='store_true', help="Plot with a dark background", dest='darkMode')
+    parser.add_argument('-n','--plot-num', action='store', help="Number of items to plot", dest='plotNum', default=20, type=int)
 
-
+    #subparsers = parser.add_subparsers(help='subplot')
     args = parser.parse_args()
+    if args.includePodcasts and not args.extended:
+        parser.error("Podcasts cannot be included or excluded unless using extended data")
     if args.song:
         searchType = 'trackName'
     else:
         searchType = 'artistName'
-    playtime = create_playtime_dict(args.path, searchType)
-    create_csv(playtime, searchType, args.outfile)
-    if args.plot:
-        plot_data(playtime, searchType)
+    sd = SpotifyData(args.path, searchType, args.extended, args.includePodcasts, args.darkMode)
+    if args.outfile:
+        sd.create_csv(args.outfile)
+    sd.plot_data(args.plotNum)
